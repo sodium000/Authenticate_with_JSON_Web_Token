4d0bcc256ccf94a588587c510cc0a2f021241746
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-04-09 22:51:30 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const todoHandler = require("./router/todoHandler");
+const userHandler = require("./router/routeHandler");
+
+// express app initialization
+const app = express(); 
+dotenv.config()
+app.use(express.json());
+
+// database connection with mongoose
+mongoose
+  .connect("mongodb://localhost/todos", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connection successful"))
+  .catch((err) => console.log(err));
+
+// application routes
+app.use("/todo", todoHandler);
+app.use("/user", userHandler);
+
+// default error handler 
+const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: err });
+}
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log("app listening at port 3000");
+});
